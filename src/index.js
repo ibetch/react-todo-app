8ed34966019a77f583fb7991e18ab873b5b04646
Khristine Tugangui <ibetch@gmail.com>
2019-08-15 11:50:26 -0700

    refactor(index): split code - create Root.js - move provider and router to Root.js - set Proptypes in Root

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducers from './reducers/reducers';
-import './index.css';
-import App from './components/App';
+import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 const store = createStore(rootReducers);
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
